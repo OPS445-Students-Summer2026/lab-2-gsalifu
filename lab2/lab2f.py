644ed3e0e74b83abbf8d47da91ff91e17fe6dd79
Gabriel Salifu <gsalifu@myseneca.ca>
2026-05-22 20:15:59 -0400

    completed Lab 2

---
+++ lab2/lab2f.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# Author: Gabriel Salifu
+# Author ID: gsalifu
+# Date Created: 2026/05/21
+
+import sys
+
+timer = int(sys.argv[1])
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+
+print("blast off!")
